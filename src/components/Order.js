004842720c22a207ab0da9b56eb3b37abd2a86b2
sumukh shetty <sumukhshetty1@sumukhs-MacBook-Pro.local>
2016-11-09 10:33:55 +0530

    initial version

---
+++ src/components/Order.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+class Order extends React.Component {
+
+
+  render() {
+    const orderIds = Object.keys(this.props.order);
+    const total = orderIds.reduce((prevTotal, key ) => {
+        const fish = this.props.fishes[key];
+        const count = this.props.order[key];
+        const isAvailable = fish && fish.status === 'available' ;
+          if (isAvailable) {
+            return prevTotal + (count*fish.price || 0)
+          }
+          return prevTotal;
+    } , 0);
+     return (
+      <div className="order-wrap">
+        <h2>Youre Order</h2>
+        <p>{orderIds}</p>
+      </div>
+
+    )
+  }
+}
+
+export default Order;
